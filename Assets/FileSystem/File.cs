c44c4e2ff7489038e713232787e728102b8cb7c9
Fran <franciscofernandezmanyas@gmail.com>
2018-08-31 18:59:40 +0200

    ls and directory system

---
+++ Assets/FileSystem/File.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class File : Inode {
+
+	public string value;
+
+	public File(string name, string value = "", string creator = "admin", string owner = "admin", bool[] permissions = null) {
+		this.name = name;
+		this.value = value;
+		this.creator = creator;
+		this.owner = owner;
+
+		if (permissions == null) this.permissions = Utils.PermissionDefault;
+		else this.permissions = permissions;
+	}
+
+}
---
+++ Assets/FileSystem/Inode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simple Inode representation, no weight and pages.
+/// </summary>
+public class Inode {
+	
+	public bool[] permissions;
+
+	public string owner;
+	public string creator;
+	public string name;
+
+	public DateTime creation;
+
+	public Inode() {
+
+	}
+
+	public Inode(string name, string creator = "admin", string owner = "admin", bool[] permissions = null) {
+		this.name = name;
+		this.creator = creator;
+		this.owner = owner;
+
+		if (permissions == null) this.permissions = Utils.PermissionDefault;
+		else this.permissions = permissions;
+	}
+}
