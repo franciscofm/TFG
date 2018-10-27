75f8ad10134157d5a3de7d4d7b4590996fddcee1
Fran <franciscofernandezmanyas@gmail.com>
2018-10-27 15:13:19 +0200

    Scenes work

--- Assets/Configuration/Configuration.cs
+++ Assets/Configuration/Configuration.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Configuration : MonoBehaviour {
+public static class Configuration {
 
-	// Use this for initialization
-	void Start () {
-		
+	public static float volume_master;
+	public static float volume_effects;
+	public static float volume_music;
+	public static float volume_voice;
+
+	public enum Lenguage { Spanish, Catalan, English };
+	public static Lenguage lenguage;
+
+	public static bool LoadConfiguration() {
+
+		return true;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public static bool EraseConfiguration() {
+
+		return true;
+	}
+
+	public static bool ChangeLenguage(Lenguage lenguage) {
+
+		return true;
 	}
 }
