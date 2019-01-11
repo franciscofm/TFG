cf7b26fb5d196bee3e23dacd57d2b799b2b0e3c1
Fran <developers@taptaptales.com>
2019-01-11 18:51:09 +0100

    Node main rework animation

---
+++ Assets/_General/LineRendererEditorCreator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ColliderLineRendererFull))]
+[ExecuteInEditMode]
+public class LineRendererEditorCreator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<ColliderLineRendererFull> ().CreateBorders ();
+	}
+
+}
